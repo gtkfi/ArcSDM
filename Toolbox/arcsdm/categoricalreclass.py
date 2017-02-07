ba35f85e8bd08734a2d357a92ff784b2e083e3c2
floodi <janne@kallunki.net>
2017-02-07 12:47:51 +0200

    categorical reclass cleanup

--- Toolbox/arcsdm/categoricalreclass.py
+++ Toolbox/arcsdm/categoricalreclass.py
@@ -2,10 +2,11 @@
 import sys, string, os, math, traceback, math
 import arcpy
 from arcpy.sa import Reclassify, Float, Divide
+from arcsdm.common import addToDisplay
 
 def Calculate(self, parameters, messages):
     try:
-        messages.addMessage("Starting categorical reclass calculation");
+        messages.addMessage("Starting categorical reclass calculation")
         cat_evidence = parameters[0].valueAsText
         reclass_field = parameters[1].valueAsText
         reclassification = parameters[2].value
@@ -14,31 +15,13 @@ def Calculate(self, parameters, messages):
         reclassified = Reclassify(cat_evidence, reclass_field, reclassification, "DATA")
         float_reclassified = Float(reclassified)
         result_raster = Divide(float_reclassified, divisor)
+        arcpy.MakeRasterLayer_management(result_raster, fm_categorical)
+        arcpy.SetParameterAsText(3, fm_categorical)
         rasterLayerName = os.path.split(fm_categorical)[1]
         addToDisplay(result_raster, rasterLayerName, "BOTTOM")
-    except Exception as Msg:
-        # get the traceback object
+    except:
         tb = sys.exc_info()[2]
-        # tbinfo contains the line number that the code failed on and the code from that line
-        tbinfo = traceback.format_tb(tb)[0]
-        # concatenate information together concerning the error into a message string
-        pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n    " + \
-            str(sys.exc_type)+ ": " + str(sys.exc_value) + "\n"
-        messages.addErrorMessage(pymsg); #msgs)
-        # print messages for use in Python/PythonWin
-        print (pymsg)
-        raise
-    return
+        errors = traceback.format_exc()
+        arcpy.AddError(errors)
 
-def addToDisplay(layer, name, position):
-    result = arcpy.MakeRasterLayer_management(layer, name)
-    lyr = result.getOutput(0)
-    product = arcpy.GetInstallInfo()['ProductName']
-    if "Desktop" in product:
-        mxd = arcpy.mapping.MapDocument("CURRENT")
-        dataframe = arcpy.mapping.ListDataFrames(mxd)[0]
-        arcpy.mapping.AddLayer(dataframe, lyr, position)
-    elif "Pro" in product:
-        aprx = arcpy.mp.ArcGISProject("CURRENT")
-        m = aprx.listMaps("Map")[0]
-        m.addLayer(lyr, position)
+
