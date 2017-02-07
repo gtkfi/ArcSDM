95e1bb2c0c60df6c8c3e720bbad9f227494e72fc
floodi <janne@kallunki.net>
2017-02-07 12:49:14 +0200

    categorical membership cleanup

--- Toolbox/arcsdm/categoricalmembership.py
+++ Toolbox/arcsdm/categoricalmembership.py
@@ -2,10 +2,11 @@
 import sys, string, os, math, traceback, math
 import arcpy
 from arcpy.sa import ReclassByTable, Float, Divide
+from arcsdm.common import addToDisplay
 
 def Calculate(self, parameters, messages):
-    messages.addMessage("Starting categorical membership calculation")
     try:
+        messages.addMessage("Starting categorical membership calculation")
         cat_evidence = parameters[0].valueAsText
         reclassification = parameters[1].valueAsText
         rescale_constant = parameters[2].value
@@ -17,30 +18,7 @@ def Calculate(self, parameters, messages):
         result_raster = Divide(float_reclass_cat_evidence, rescale_constant)
         rasterLayerName = os.path.split(fmcat)[1]
         addToDisplay(result_raster, rasterLayerName, "BOTTOM")
-    except Exception as Msg:
-        # get the traceback object
+    except:
         tb = sys.exc_info()[2]
-        # tbinfo contains the line number that the code failed on and the code from that line
-        tbinfo = traceback.format_tb(tb)[0]
-        # concatenate information together concerning the error into a message string
-        pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n    " + \
-            str(sys.exc_type)+ ": " + str(sys.exc_value) + "\n"
-        messages.addErrorMessage(pymsg); #msgs)
-        # print messages for use in Python/PythonWin
-        print (pymsg)
-        raise
-
-def addToDisplay(layer, name, position):
-    result = arcpy.MakeRasterLayer_management(layer, name)
-    lyr = result.getOutput(0)
-    product = arcpy.GetInstallInfo()['ProductName']
-    if "Desktop" in product:
-        mxd = arcpy.mapping.MapDocument("CURRENT")
-        dataframe = arcpy.mapping.ListDataFrames(mxd)[0]
-        arcpy.mapping.AddLayer(dataframe, lyr, position)
-    elif "Pro" in product:
-        aprx = arcpy.mp.ArcGISProject("CURRENT")
-        m = aprx.listMaps("Map")[0]
-        m.addLayer(lyr, position)
-
-
+        errors = traceback.format_exc()
+        arcpy.AddError(errors)       
