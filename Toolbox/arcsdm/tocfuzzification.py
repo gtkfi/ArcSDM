251a1072c33a1928bf4606d2d1e7495224615e82
floodi <janne@kallunki.net>
2017-02-07 12:50:53 +0200

    toc fuzzification cleanup

--- Toolbox/arcsdm/tocfuzzification.py
+++ Toolbox/arcsdm/tocfuzzification.py
@@ -2,6 +2,7 @@
 import sys, string, os, math, traceback, math
 import arcpy
 from arcpy.sa import Float, Divide, Minus, Reclassify
+from arcsdm.common import addToDisplay
 
 def Calculate(self, parameters, messages):
     try:
@@ -21,28 +22,7 @@ def Calculate(self, parameters, messages):
         denominator = Minus(classes, min_class_number)
         fmtoc = Divide(minus_float1, denominator)
         addToDisplay(fmtoc, rasterLayerName, "BOTTOM")
-    except Exception as Msg:
-        # get the traceback object
+    except:
         tb = sys.exc_info()[2]
-        # tbinfo contains the line number that the code failed on and the code from that line
-        tbinfo = traceback.format_tb(tb)[0]
-        # concatenate information together concerning the error into a message string
-        pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n    " + \
-            str(sys.exc_type)+ ": " + str(sys.exc_value) + "\n"
-        messages.addErrorMessage(pymsg); #msgs)
-        # print messages for use in Python/PythonWin
-        print (pymsg)
-        raise
-
-def addToDisplay(layer, name, position):
-    result = arcpy.MakeRasterLayer_management(layer, name)
-    lyr = result.getOutput(0)
-    product = arcpy.GetInstallInfo()['ProductName']
-    if "Desktop" in product:
-        mxd = arcpy.mapping.MapDocument("CURRENT")
-        dataframe = arcpy.mapping.ListDataFrames(mxd)[0]
-        arcpy.mapping.AddLayer(dataframe, lyr, position)
-    elif "Pro" in product:
-        aprx = arcpy.mp.ArcGISProject("CURRENT")
-        m = aprx.listMaps("Map")[0]
-        m.addLayer(lyr, position)
+        errors = traceback.format_exc()
+        arcpy.AddError(errors)         
