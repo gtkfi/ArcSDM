5aeb7beda235491a945354356f0f54c39a3c42a0
emmu.taivalsaari <emmu.taivalsaari@gtk.fi>
2023-12-29 16:45:51 +0200

    Implement thinning selection. Reduce complexity of code: remove an unnecessary SearchCursor call + remove old versions of the algorithm.

--- Toolbox/arcsdm/sitereduction.py
+++ Toolbox/arcsdm/sitereduction.py
@@ -32,6 +32,7 @@ def ReduceSites(self, parameters, messages):
 
         messages.AddMessage(f"Training points: {input_features}")
 
+        # Select the features, applying mask if it is set
         mask = arcpy.env.mask
         if mask:
             if not arcpy.Exists(mask):
@@ -60,16 +61,44 @@ def ReduceSites(self, parameters, messages):
 
         training_sites = dict()
 
-        with arcpy.da.SearchCursor(input_features, identifier) as features:
-            for feature in features:
-                training_sites[random.random()] = feature[0]
+        # Go through the selected features and collect attributes that are used later into training sites
+        with arcpy.da.SearchCursor(input_features, [identifier, "SHAPE@XY"]) as features:
+            if is_thinning_selection_selected:
+                thinned_points = []
+                first_feature = next(features)
+                thinned_points.append(Point(first_feature[1]))
 
-        training_sites = dict(sorted(training_sites.items()))
+                clause = f"{identifier} = {first_feature[0]}"
 
-        # Clear selection
-        arcpy.management.SelectLayerByAttribute(input_features, "CLEAR_SELECTION")
+                if is_random_selection_selected:
+                    # A random number is used as key to allow making a random selection
+                    training_sites[random.random()] = first_feature[0]
+
+                min_dist_m = math.sqrt(unit_area_sq_km * 1000000.0 / math.pi)
+                arcpy.AddMessage(f"Minimum distance (m): {min_dist_m}")
+
+                for feature in features:
+                    point = Point(feature[1])
+                    if check_if_point_is_far_enough(thinned_points, min_dist_m, point):
+                        thinned_points.append(point)
+                        clause += f" or {identifier} = {feature[0]}"
+                        if is_random_selection_selected:
+                            training_sites[random.random()] = feature[0]
+                
+                if not is_random_selection_selected:
+                    # Create the selection
+                    arcpy.management.SelectLayerByAttribute(input_features, "CLEAR_SELECTION")
+                    arcpy.management.SelectLayerByAttribute(input_features, "ADD_TO_SELECTION", clause)
+            else:
+                for feature in features:
+                    # A random number is used as key to allow making a random selection
+                    training_sites[random.random()] = feature[0]
 
         if is_random_selection_selected:
+            # Clear selection
+            arcpy.management.SelectLayerByAttribute(input_features, "CLEAR_SELECTION")
+
+            training_sites = dict(sorted(training_sites.items()))            
             partition = percentage / 100.0
             cutoff = int(partition * len(training_sites)) # Rounded down to ensure within index
             
@@ -89,8 +118,9 @@ def ReduceSites(self, parameters, messages):
 
             arcpy.management.SelectLayerByAttribute(input_features, "ADD_TO_SELECTION", clause)
         
+        # Copy the selected features to a new layer
         if output_features:
-                arcpy.management.CopyFeatures(input_features, output_features)
+            arcpy.management.CopyFeatures(input_features, output_features)
 
         # Finally, clear selection
         arcpy.management.SelectLayerByAttribute(input_features, "CLEAR_SELECTION")
@@ -120,3 +150,58 @@ def get_identifier(input_features):
         return "FID"
     
     raise arcpy.ExecuteError("Check input feature attributes! The training points have no OBJECTID or FID.")
+
+
+class Point:
+    def __init__(self, point):
+        self.x = point[0]
+        self.y = point[1]
+
+    def __eq__(self, otherpnt):
+        return self.x == otherpnt.x and self.y == otherpnt.y
+    
+    # the __cmp__() special method is no longer honored in Python 3
+    # TODO: remove/replace
+    def __cmp__(self, otherpnt):
+        return 0 if (self.x == otherpnt.x and self.y == otherpnt.y) else 1
+        # if self.x == otherpnt.x and self.y == otherpnt.y:
+        #     return 0
+        # else:
+        #     return 1
+    
+    def __repr__(self):
+        return f"{self.x}, {self.y}"
+
+
+def rowgen(searchcursor_rows):
+    """ Convert gp searchcursor to a generator function """
+    rows = searchcursor_rows
+    row = rows.next()        
+    while row:
+        yield row
+        row = rows.next()
+
+
+def distance(pnt1, pnt0):
+    return math.hypot(pnt1.x - pnt0.x, pnt1.y - pnt0.y)
+
+
+def check_if_point_is_far_enough(points, limit, new_point):
+    """
+        1. Add first point to saved list.
+        2. Check if next point is within Unit radius of saved points.
+        3. If not, add it to saved list.
+        4. Go to 2.
+        
+        The number tried is n/2 on average, because saved list grows from 1.
+        nTrials = Sigma(x,x=(1,n)) = n/2 + n*n/4 or O(n*n)
+        nTrials(10) = 5 + 25 = 30
+        nTrials(100) = 50 + 2500 = 2550
+        nTrials(1000) = 500 + 250,000 = 250,500
+        nTrials(10,000) = 5000 + 25,000,000 = 25,050,000
+    """
+    for point in points:
+        d = distance(point, new_point)
+        if d < limit:
+            return False
+    return True
