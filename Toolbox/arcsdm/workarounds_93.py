7c4dab9558a6921516b4725783e8aabe891e3723
tomironkko <30999998+tomironkko@users.noreply.github.com>
2024-11-08 11:17:43 +0200

    Convert to use arcpy instead of arcgisscripting. (#170)
    * Convert to use arcpy instead of arcgisscripting.
    
    * Refactoed code accorfing to Pyry's suggestions in Pull Request comments.

--- Toolbox/arcsdm/workarounds_93.py
+++ Toolbox/arcsdm/workarounds_93.py
@@ -13,145 +13,101 @@ Work-arounds for 9.3 version of ArcGIS
 """
 import sys, os, traceback, arcpy
 
-
-def rowgen( searchcursor ):
-    """ wrapper for searchcursor to permit its use in Python for statement """
-    rows = searchcursor
-    rows.reset()
-    row = rows.next()
-    while row:
-        yield row
-        row = rows.next()
-    del rows
-
-    
-    
-#This function needs to be GENERAL    
+def rowgen(searchcursor):
+    """ 
+    Wrapper for searchcursor to permit its use in Python for statement.
+    This function yields rows from the search cursor one by one.
+    """
+    with searchcursor as rows:
+        for row in rows:
+            yield row
 
 def GetIDField(table):
-    field_names = []
-    fields = arcpy.ListFields(table,"")
-    for field in fields:
-        field_names.append(field.name)
+    """
+    Returns the ID field name of the given table.
+    Checks for the presence of 'FID' or 'OBJECTID' fields.
+    
+    Args:
+        table (str): The table to check for ID fields.
+    
+    Returns:
+        str: The name of the ID field.
+    """
+    field_names = [field.name for field in arcpy.ListFields(table)]
     if "FID" in field_names:
         return "FID"
     else:
         return "OBJECTID" 
     
-def ExtractValuesToPoints(gp, inputRaster, inputFeatures, siteFIDName):
-    """ ExtractValuesToPoints tool in ArcGIS 9.3 now selects ALL features.
+def ExtractValuesToPoints(inputRaster, inputFeatures, siteFIDName):
+    """
+    Extracts values to points for selected features in ArcGIS 9.3.
     This routine generates the extracted feature class from only selected input features.
     If the selected subset is very large, a very large query string will be created in memory.
+    
+    Args:
+        inputRaster (str): The input raster dataset.
+        inputFeatures (str): The input feature class.
+        siteFIDName (str): The field name to store the FID values.
+    
+    Returns:
+        str: The path to the temporary extracted shapefile.
     """
-
     try:
-        #arcpy.AddMessage("Debug: workarounds93 Extracting values to points...")
         assert siteFIDName in ('TPFID','NDTPFID')
-        if siteFIDName not in [field.name for field in rowgen(gp.ListFields(inputFeatures))]:
-            gp.AddField_management(inputFeatures, siteFIDName, 'LONG')            
-            #gp.AddMessage("Debug: workarounds93 Added new FID field");           
-        #else:
-            #arcpy.AddMessage("Debug: workarounds93 SiteFIDName = " + siteFIDName );
-         
-        #gp.CalculateField_management(inputFeatures, siteFIDName, "!FID!", "PYTHON_9.3", None)
-        idfield = GetIDField(inputFeatures); 
-        gp.CalculateField_management(inputFeatures, siteFIDName, "!{}!".format(idfield) , "PYTHON_9.3")
+        if siteFIDName not in [field.name for field in arcpy.ListFields(inputFeatures)]:
+            arcpy.AddField_management(inputFeatures, siteFIDName, 'LONG')            
 
-        tempExtrShp = gp.CreateScratchName ('Extr', 'Tmp', 'shapefile', gp.scratchworkspace)
-        #arcpy.AddMessage("Debug: gp.scratchworkspace = " + gp.scratchworkspace);
-        #arcpy.AddMessage("Debug: tempExtrShp = " + tempExtrShp);
-        #arcpy.AddMessage("Debug: inputFeatures = " + inputFeatures);
-        #arcpy.AddMessage("Debug: inputRaster = " + inputRaster);
+        idfield = GetIDField(inputFeatures)
+        arcpy.CalculateField_management(inputFeatures, siteFIDName, "!{}!".format(idfield), "PYTHON_9.3")
 
-        #tempSelectedExtrShp = gp.CreateScratchName ('SelExtr', 'Tmp', 'shapefile', gp.scratchworkspace)
-        gp.ExtractValuesToPoints_sa(inputFeatures, inputRaster, tempExtrShp)
-        #gp.addwarning(str(gp.getcount(inputFeatures)))
-##        if gp.GetCount_management(inputFeatures) < gp.GetCount_management(tempExtrShp):
-##            #Not going to happen in 9.2, but will in 9.3
-##            rows = rowgen(gp.searchcursor(inputFeatures)) # Gets selected rows
-##            FIDs = (str(row.FID) for row in rows)
-##            query = '"FID" IN (' + ','.join(FIDs) + ')'
-##            #gp.addwarning(query)
-##            TempShp = 'tempshp'
-##            gp.MakeFeatureLayer_management(tempExtrShp, TempShp)
-##            gp.addwarning(str(gp.GetCount(TempShp)))
-##            gp.SelectLayerByAttribute_management(tempExtrShp, "NEW_SELECTION", query)
-##            #gp.addwarning(str(gp.GetCount(TempShp)))
-##            return tempExtrShp
-##        else:
-##            return inputFeatures
+        tempExtrShp = arcpy.CreateScratchName('Extr', 'Tmp', 'shapefile', arcpy.env.scratchWorkspace)
+        arcpy.sa.ExtractValuesToPoints(inputFeatures, inputRaster, tempExtrShp)
         return tempExtrShp
     except:
-        # get the traceback object
         tb = sys.exc_info()[2]
-        # tbinfo contains the line number that the code failed on and the code from that line
         tbinfo = traceback.format_tb(tb)[0]
-        # concatenate information together concerning the error into a message string
         pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n    " + \
-            str(sys.exc_info()) + "\n"    #AL 050520
-        #    str(sys.exc_type)+ ": " + str(sys.exc_value) + "\n"
-        # generate a message string for any geoprocessing tool errors
-        msgs = "GP ERRORS:\n" + gp.GetMessages(2) + "\n"
-        gp.AddError(msgs)
+            str(sys.exc_info()) + "\n"
+        msgs = "GP ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+        arcpy.AddError(msgs)
+        arcpy.AddError(pymsg)
+        print(pymsg)
+        print(msgs)
 
-        # return gp messages for use with a script tool
-        gp.AddError(pymsg)
-
-        # print messages for use in Python/PythonWin
-        print (pymsg)
-        print (msgs)
-
-def BandCollectionStats(gp, inputRasters, BandCol_Stats, state="BRIEF"):
-    """ BandCollectionStatistics tool in ArcGIS 9.3 now does not use mask; in fact,
-    the tool breaks when a mask is set in the environment.
-    This routine not creates masked copies of all input rasters, temporarily turns off
-    the environment mask, runs the Band Collection Statistics tool on them.
+def BandCollectionStats(inputRasters, BandCol_Stats, state="BRIEF"):
+    """
+    Computes band collection statistics for input rasters.
+    This routine creates masked copies of all input rasters, temporarily turns off
+    the environment mask, and runs the Band Collection Statistics tool on them.
+    
+    Args:
+        inputRasters (list): List of input raster datasets.
+        BandCol_Stats (str): The output band collection statistics file.
+        state (str): The state of the statistics, default is "BRIEF".
     """
     try:
-##>>>>>>>>>>>>>  Code when using pre-masked data sets >>>>>>>>>>>>>
-        saved_mask = gp.mask
-        gp.mask = ''
-        gp.BandCollectionStats_sa(inputRasters, BandCol_Stats, state)
-        gp.mask = saved_mask
-##>>>>>>>>>>>>>  Code when using unmasked data sets >>>>>>>>>>>>>
-##        OutRasters = []
-##        InMask = gp.mask
-##        for InRaster in inputRasters.split(";"):
-##            OutRaster = gp.CreateScratchName("BCS", "TMP", "RasterBand", gp.scratchworkspace)
-##            gp.ExtractByMask_sa(InRaster, InMask, OutRaster)
-##            OutRasters.append(OutRaster)
-        
-##        saved_mask = gp.mask
-##        gp.mask = ''
-##        gp.BandCollectionStats_sa(";".join(OutRasters), BandCol_Stats, state)
-##        gp.mask = saved_mask
-##        for OutRaster in OutRasters:
-##            gp.Delete(OutRaster)
-##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<          
+        saved_mask = arcpy.env.mask
+        arcpy.env.mask = ''
+        arcpy.sa.BandCollectionStats(inputRasters, BandCol_Stats, state)
+        arcpy.env.mask = saved_mask
     except:
-        # Get the geoprocessing error messages
-        #
-        msgs = gp.GetMessage(0)
-        msgs += gp.GetMessages(2)
-
-        # Return gp error messages for use with a script tool
-        #
-        gp.AddError(msgs)
-
-        # Print gp error messages for use in Python/PythonWin
-        #
+        msgs = arcpy.GetMessages(0)
+        msgs += arcpy.GetMessages(2)
+        arcpy.AddError(msgs)
         raise
 
 __all__ = ["ExtractValuesToPoints","BandCollectionStats"]
 
-def testBCS(gp):
-    inputrasters = gp.GetParameterAsText(0)
-    outputfile = gp.GetParameterAsText(1)
-    BandCollectionStats(gp, inputrasters, outputfile)
-    gp.SetParameterAsText(1, outputfile)
+def testBCS():
+    """
+    Test function for BandCollectionStats.
+    Gets input parameters from the user, runs BandCollectionStats, and sets the output parameter.
+    """
+    inputrasters = arcpy.GetParameterAsText(0)
+    outputfile = arcpy.GetParameterAsText(1)
+    BandCollectionStats(inputrasters, outputfile)
+    arcpy.SetParameterAsText(1, outputfile)
     
 if __name__ == '__main__':
-    import arcgisscripting
-    gp = arcgisscripting.create()
-
-    testBCS(gp)
+    testBCS()
