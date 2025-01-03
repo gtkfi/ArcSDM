88a0197b3cce7ae5969bf1008b669255349ae6a8
emmu.taivalsaari <emmu.taivalsaari@gtk.fi>
2025-01-03 17:10:03 +0200

    Quick clean-up to common.py.

--- Toolbox/arcsdm/common.py
+++ Toolbox/arcsdm/common.py
@@ -1,60 +1,59 @@
 # -*- coding: utf-8 -*-
 
-# ArcSDM 5 Common and general functions
+# ArcSDM 6 Common and general functions
 #
 # Janne Kallunki, GTK, 2017
 # Tero Ronkko, GTK, 2018
 # Arto Laiho, GTK, 29.4.2020 (addToDisplay)
 
-import sys, os, traceback
 import arcpy
+import os
+import sys
+import traceback
+
 import arcsdm.config as cfg
-from arcsdm.exceptions import SDMError
 
 PY2 = sys.version_info[0] == 2
 PY34 = sys.version_info[0:2] >= (3, 4)
 
 if PY2:
-    from imp import reload;
+    from imp import reload
 if PY34:
     import importlib
 
     
 def testandwarn_arcgispro():
-    installinfo = arcpy.GetInstallInfo ();
-    if (installinfo['ProductName'] == "ArcGISPro"):
-        arcpy.AddWarning("This tool does not work properly on ArcGISPro!");
-        return True;
+    installinfo = arcpy.GetInstallInfo()
+
+    if installinfo["ProductName"] == "ArcGISPro":
+        arcpy.AddWarning("This tool does not work properly on ArcGISPro!")
+        return True
     else:
-        return False;
-        
-        
+        return False
+
+
 def testandwarn_filegeodatabase_environment():
     desc = arcpy.Describe(arcpy.env.workspace)
-    #arcpy.AddMessage("%-20s %s (%s)" % ("Workspace: ", arcpy.env.workspace, desc.workspaceType));
-    wdesc = arcpy.Describe(arcpy.env.scratchWorkspace)       
-    #arcpy.AddMessage("%-20s %s (%s)" % ("Scratch workspace:",  arcpy.env.scratchWorkspace, wdesc.workspaceType))
-    if desc.workspaceType == "LocalDatabase" or wdesc.workspaceType == "LocalDatabase":
-        arcpy.AddWarning("For this tool workspaces cannot be filegeodatabases!");
-        return True;
+    wdesc = arcpy.Describe(arcpy.env.scratchWorkspace)
+
+    if (desc.workspaceType == "LocalDatabase") or (wdesc.workspaceType == "LocalDatabase"):
+        arcpy.AddWarning("For this tool workspaces cannot be filegeodatabases!")
+        return True
     else:
-        return False;
-   
+        return False
+
+
 def testandwarn_filegeodatabase_source(resourcename):
-    desc = arcpy.Describe(resourcename);
-    #arcpy.AddMessage(desc.catalogpath);
-    workspace = os.path.dirname(desc.catalogpath)
-    #arcpy.AddMessage(workspace);        
+    desc = arcpy.Describe(resourcename)
+    workspace = os.path.dirname(desc.catalogpath)  
     if [any(ext) for ext in ('.gdb', '.mdb', '.sde') if ext in os.path.splitext(workspace)]:
         workspace = workspace;
-        arcpy.AddWarning("For this tool the source data cannot be in geodatabase format!");        
-        return True;
+        arcpy.AddWarning("For this tool the source data cannot be in geodatabase format!")
+        return True
     else:
-        return False;
-    
+        return False
 
-   
-    
+
 def reload_arcsdm_modules(messages):
     arcsdm_modules = [m.__name__ for m in sys.modules.values() if m and m.__name__.startswith(__package__)]
     for m in arcsdm_modules:
@@ -64,11 +63,13 @@ def reload_arcsdm_modules(messages):
             messages.AddMessage("Failed to reload module %s. Reason:%s" %(m, e.message))
     messages.AddMessage("Reloaded %s modules" % __package__)
 
+
 def reload_module(name):
     if PY2:
         reload(name)
     if PY34:
         importlib.reload(name)
+
 
 def execute_tool(func, self, parameters, messages):
     if cfg.RELOAD_MODULES:
@@ -90,7 +91,8 @@ def execute_tool(func, self, parameters, messages):
     except:
         tb = sys.exc_info()[2]
         errors = "Unhandled exception caught\n" + traceback.format_exc()
-        arcpy.AddError(errors)         
+        arcpy.AddError(errors)
+
 
 def addToDisplay(layer, name, position):
     result = arcpy.MakeRasterLayer_management(layer, name)
@@ -102,9 +104,7 @@ def addToDisplay(layer, name, position):
         arcpy.mapping.AddLayer(dataframe, lyr, position)
     elif "Pro" in product:
         aprx = arcpy.mp.ArcGISProject("CURRENT")
-        m = aprx.listMaps("Map")[0] 
-        #m.addLayer(lyr, position)
-        #AL removed row above and added next three rows
+        m = aprx.listMaps("Map")[0]
         m.addDataFromPath(layer)
         layer0 = m.listLayers()[0]
         layer0.name = name
@@ -128,10 +128,6 @@ def log_arcsdm_details():
 
     wdesc = arcpy.Describe(arcpy.env.scratchWorkspace)
     arcpy.AddMessage("%-20s %s (%s)" % ("Scratch workspace:", arcpy.env.scratchWorkspace, wdesc.workspaceType))
-
-    # if wdesc.workspaceType != desc.workspaceType:
-    #     arcpy.AddError("Workspace and scratch workspace must be of the same type!")
-    #     raise arcpy.ExecuteError("Workspace type mismatch")
 
 
 def select_features_by_mask(input_feature):
