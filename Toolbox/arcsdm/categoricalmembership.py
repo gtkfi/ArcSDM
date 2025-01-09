bae19612178ce32dafc81b234b8a3437df97de97
tomironkko <30999998+tomironkko@users.noreply.github.com>
2025-01-09 10:17:10 +0200

    Migrate functionalities to ArcGIS Pro 3.x

--- Toolbox/arcsdm/categoricalmembership.py
+++ Toolbox/arcsdm/categoricalmembership.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 """
+ArcSDM 6 ToolBox for ArcGIS Pro
 
-    ArcSDM 5 - ArcSDM for ArcGis pro
+Conversion and tool development for ArcGIS Pro by Geological Survey of Finland (GTK), 2024.
 
+    
     History:
+
+    ArcSDM 5 - ArcSDM for ArcGIS Pro:
     29.4.2020 fixed reclassification table field names / Arto Laiho, GTK/GSF
 """
+
 import sys, string, os, math, traceback, math
 import arcpy
 from arcpy.sa import ReclassByTable, Float, Divide
@@ -20,13 +25,12 @@ def Calculate(self, parameters, messages):
         fmcat = parameters[3].valueAsText
         if fmcat == '#' or not fmcat:
             fmcat = "%Workspace%/FMCat" # provide a default value if unspecified
-        #Next row doesn't work with Reclassification table from Categorical & Reclass / Arto Laiho, Geological survey of Finland
-        #reclass_cat_evidence = ReclassByTable(cat_evidence, reclassification, "VALUE", "VALUE", "FMx100", "NODATA")
-        product = arcpy.GetInstallInfo()['ProductName']
-        if "Desktop" in product:
-            reclass_cat_evidence = ReclassByTable(cat_evidence, reclassification, "FROM", "TO", "OUT", "NODATA") #AL 290420
-        else:
-            reclass_cat_evidence = ReclassByTable(cat_evidence, reclassification, "FROM_", "TO", "OUT", "NODATA") #AL 290420
+
+        # Next row doesn't work with Reclassification table from Categorical & Reclass / Arto Laiho, Geological survey of Finland
+
+        # Updated to use ArcGIS Pro only
+        reclass_cat_evidence = ReclassByTable(cat_evidence, reclassification, "FROM_", "TO", "OUT", "NODATA")
+
         float_reclass_cat_evidence = Float(reclass_cat_evidence)
         result_raster = Divide(float_reclass_cat_evidence, rescale_constant)
         rasterLayerName = os.path.split(fmcat)[1]
@@ -34,4 +38,4 @@ def Calculate(self, parameters, messages):
     except:
         tb = sys.exc_info()[2]
         errors = traceback.format_exc()
-        arcpy.AddError(errors)       
+        arcpy.AddError(errors)
