c8c00455bd89193b506fcbae027c047957be16e1
lehtonenp <pyry.lehtonen@gtk.fi>
2025-11-21 15:32:33 +0200

    Add coordinate field names to csi

--- Toolbox/arcsdm/cosine_similarity_index.py
+++ Toolbox/arcsdm/cosine_similarity_index.py
@@ -9,14 +9,15 @@ def execute(self, parameters, messages):
         labelled_path = parameters[0].valueAsText
         csv_nodata = float(parameters[1].value) if parameters[1].value else -9999.0
         label_field_names = parameters[2].valueAsText.split(';') if parameters[2].valueAsText else []
-        feature_field_names = parameters[3].valueAsText.split(';') if parameters[3].valueAsText else None
-        evidence_type = parameters[4].valueAsText if parameters[4].value else None
-        rasters_list = parameters[5].valueAsText.split(';') if parameters[5].valueAsText else []
-        out_labelled_pairwise_csv = parameters[7].valueAsText
-        out_class_centroid = parameters[8].valueAsText if parameters[8].value else None
-        out_evidence_table_csv = parameters[9].valueAsText if parameters[9].value else None
-        out_raster_folder = parameters[10].valueAsText if parameters[10].value else None
-        
+        coordinate_field_names = parameters[3].valueAsText.split(';') if parameters[3].valueAsText else []
+        feature_field_names = parameters[4].valueAsText.split(';') if parameters[4].valueAsText else []
+        evidence_type = parameters[5].valueAsText if parameters[5].value else None
+        rasters_list = parameters[6].valueAsText.split(';') if parameters[6].valueAsText else []
+        vector_list = parameters[7].valueAsText.split(';') if parameters[7].valueAsText else []
+        out_labelled_pairwise_csv = parameters[8].valueAsText
+        out_class_centroid = parameters[9].valueAsText if parameters[9].value else None
+        out_evidence_table_csv = parameters[10].valueAsText if parameters[10].value else None
+        out_raster_folder = parameters[11].valueAsText if parameters[11].value else None
         # Filter labeled points by field value
         selected_label_field = parameters[11].valueAsText if len(parameters) > 11 and parameters[11].value else None
         
@@ -27,6 +28,7 @@ def execute(self, parameters, messages):
             selected_label_field,
             labelled_path,
             label_field_names,
+            coordinate_field_names,
             feature_field_names,
             rasters_list,
             evidence_type,
